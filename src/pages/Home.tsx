960ee9e0421d922c4abf56bf38a6bd471f27be45
lonelythoughts-4 <ghostgpt4@proton.me>
2026-02-12 11:25:40 +0100

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import Trade from './Trade';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-const Home = () => {
-  return <Trade />;
+const Home: React.FC = () => {
+  const location = useLocation();
+  const currentPage =
+    location.pathname === '/support'
+      ? 'support'
+      : location.pathname === '/trial'
+        ? 'trial'
+        : 'access';
+
+  return (
+    <div className="min-h-screen bg-slate-950 flex flex-col">
+      <Header currentPage={currentPage} />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
